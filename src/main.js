229bc49fe2c2c5ff2434ccae54ad952c8d8477c7
Rakib <Rakib@Gmail.com>
2024-05-26 14:01:20 +0300

    updat clock

---
+++ src/main.js
@@ -0,0 +1,50 @@
+const box = document.querySelector(".box");
+const box1 = document.querySelector(".box1");
+const pTeg = document.querySelector(".box1 p");
+const heading = document.querySelector(".box h1");
+
+setInterval(() => {
+  timerFunction();
+  DatFunction();
+}, 1000);
+
+const timerFunction = () => {
+  const date = new Date();
+
+  heading.innerHTML = `
+   
+   ${date.getHours() > 12 ? date.getHours() - 12 : date.getHours()}:${
+    date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes()
+  }:${date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds()}:${
+    date.getHours() > 12 ? "<span> PM </span>" : "<span> AM </span>"
+  }
+   `;
+};
+const monthList = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const DatFunction = () => {
+  const date = new Date();
+
+  pTeg.innerHTML = `
+   ${date.getDate()} : ${monthList[date.getMonth()]} : ${date.getFullYear()}
+   `;
+};
+DatFunction();
+
+setInterval(() => {
+  timerFunction();
+  DatFunction();
+}, 1000);
